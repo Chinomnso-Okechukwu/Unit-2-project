7bb59cbe5f9d686574c8bd5948695d470ee9137a
Chinomnso-Okechukwu <54611452+Chinomnso-Okechukwu@users.noreply.github.com>
2020-01-08 11:08:18 +0900

    Create hw2.c
    This C program checks whether two integers are equal or not.

---
+++ hw2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int x,y;
+  printf("enter the values for x,y\n");
+  scanf("%d%d",&x,&y);
+  if (x==y)
+  printf("x and y are equal\n");
+  else
+  printf("x and y are not equal\n");
+  return 0;
+}
